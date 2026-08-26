6630584cc84ef250414faa6e73d7773fdc51b318
Erick Jiménez <erick9025@hotmail.com>
2026-08-26 17:10:47 -0600

    Removed "projects" node from json config, now using default chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,46 +43,6 @@ export default defineConfig({
     viewport: { width: 1980, height: 1080 } // HD resolution but is being overriden by projects config (chromiu, etc.)
   },
 
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        viewport: { width: 2560, height: 1440 }, // 2K
-      },
-    },
-
-    /*{
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },*/
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
-
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
