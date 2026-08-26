ddbb77ffea1fb99fb7d5e2d85562951f1056ac47
Erick Jiménez <erick9025@hotmail.com>
2026-08-26 16:52:06 -0600

    Saw radios, checkboxes and select

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,14 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 60_000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5_000
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -20,7 +28,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { open: 'always' }]
--- tests/testsAug26.spec.ts
+++ tests/testsAug26.spec.ts
@@ -1,30 +1,112 @@
-import { test, expect, Locator } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 const url1: string = 'https://www.testmuai.com/selenium-playground/checkbox-demo/';
 const url2: string = 'https://www.testmuai.com/selenium-playground/radiobutton-demo/';
 
-test('test wednesday aug 26', async ({ page }) => {
-  await page.goto(url1);
+test.describe('Checkboxes and Radio Buttons Tests', () => {
 
-  const checkAlone: string = "(//input[@type='checkbox'])[1]";
-  const checkVerticalGroup1: string = "//label[contains(.,'Option 1')]/child::input[@type='checkbox' and not(@name)]";
-  const checkVerticalGroup2: string = "//label[contains(.,'Option 2')]/child::input[@type='checkbox' and not(@name)]";
-  const checkVerticalGroup3: string = "//label[contains(.,'Option 3')]/child::input[@type='checkbox' and not(@name)]";
-  const checkVerticalGroup4: string = "//label[contains(.,'Option 4')]/child::input[@type='checkbox' and not(@name)]";
-  const checkHorizontalGroup1: string = "//label[contains(.,'Option 1')]/child::input[@type='checkbox' and contains(@name,'option')]";
-  const checkHorizontalGroup2: string = "//label[contains(.,'Option 2')]/child::input[@type='checkbox' and contains(@name,'option')]";
-  const checkHorizontalGroup3: string = "//label[contains(.,'Option 3')]/child::input[@type='checkbox' and contains(@name,'option')]";
-  const checkHorizontalGroup4: string = "//label[contains(.,'Option 4')]/child::input[@type='checkbox' and contains(@name,'option')]"; 
+  test('test wednesday aug 26 - CHECKBOXES', async ({ page }) => {
+    await page.goto(url1);
 
-  // Validate that all 9 checkboxes are visible
-  await expect(page.locator(checkAlone)).toBeVisible();
-  await expect(page.locator(checkVerticalGroup1)).toBeVisible();
-  await expect(page.locator(checkVerticalGroup2)).toBeVisible();
-  await expect(page.locator(checkVerticalGroup3)).toBeVisible();
-  await expect(page.locator(checkVerticalGroup4)).toBeVisible();
-  await expect(page.locator(checkHorizontalGroup1)).toBeVisible();
-  await expect(page.locator(checkHorizontalGroup2)).toBeVisible();
-  await expect(page.locator(checkHorizontalGroup3)).toBeVisible();
-  await expect(page.locator(checkHorizontalGroup4)).toBeVisible();
+    const checkAlone: string = "(//input[@type='checkbox'])[1]";
+    const checkVerticalGroup1: string = "//label[contains(.,'Option 1')]/child::input[@type='checkbox' and not(@name)]";
+    const checkVerticalGroup2: string = "//label[contains(.,'Option 2')]/child::input[@type='checkbox' and not(@name)]";
+    const checkVerticalGroup3: string = "//label[contains(.,'Option 3')]/child::input[@type='checkbox' and not(@name)]"; //should be disabled
+    const checkVerticalGroup4: string = "//label[contains(.,'Option 4')]/child::input[@type='checkbox' and not(@name)]"; //should be disabled
+    const checkHorizontalGroup1: string = "//label[contains(.,'Option 1')]/child::input[@type='checkbox' and contains(@name,'option')]";
+    const checkHorizontalGroup2: string = "//label[contains(.,'Option 2')]/child::input[@type='checkbox' and contains(@name,'option')]";
+    const checkHorizontalGroup3: string = "//label[contains(.,'Option 3')]/child::input[@type='checkbox' and contains(@name,'option')]";
+    const checkHorizontalGroup4: string = "//label[contains(.,'Option 4')]/child::input[@type='checkbox' and contains(@name,'option')]"; 
 
+    // Validate that all 9 checkboxes are visible
+    await expect(page.locator(checkAlone)).toBeVisible();
+    await expect(page.locator(checkVerticalGroup1)).toBeVisible();
+    await expect(page.locator(checkVerticalGroup2)).toBeVisible();
+    await expect(page.locator(checkVerticalGroup3)).toBeVisible();
+    await expect(page.locator(checkVerticalGroup4)).toBeVisible();
+    await expect(page.locator(checkHorizontalGroup1)).toBeVisible();
+    await expect(page.locator(checkHorizontalGroup2)).toBeVisible();
+    await expect(page.locator(checkHorizontalGroup3)).toBeVisible();
+    await expect(page.locator(checkHorizontalGroup4)).toBeVisible();
+
+    // 7 checkboxes should be enabled, 2 checkboxes should be disabled
+    await expect(page.locator(checkAlone)).toBeEnabled();
+    await expect(page.locator(checkVerticalGroup1)).toBeEnabled();
+    await expect(page.locator(checkVerticalGroup2)).toBeEnabled();
+    await expect(page.locator(checkVerticalGroup3)).toBeDisabled(); //should be disabled
+    await expect(page.locator(checkVerticalGroup4)).toBeDisabled(); //should be disabled
+    await expect(page.locator(checkHorizontalGroup1)).toBeEnabled();
+    await expect(page.locator(checkHorizontalGroup2)).toBeEnabled();
+    await expect(page.locator(checkHorizontalGroup3)).toBeEnabled();
+    await expect(page.locator(checkHorizontalGroup4)).toBeEnabled();
+
+    //await expect(page.locator(checkAlone)).toBeChecked(); // trigger an error
+
+    // Click alone
+    await page.locator(checkAlone).click(); // check
+
+    // Check that the checkbox is checked
+    await expect(page.locator(checkAlone)).toBeChecked();
+
+    // Click again to uncheck
+    await page.locator(checkAlone).click(); // uncheck
+
+    // Check that the checkbox is unchecked
+    await expect(page.locator(checkAlone)).not.toBeChecked();
+
+    // Remaining 8 checkboxes are unchecked
+    await expect(page.locator(checkVerticalGroup1)).not.toBeChecked();
+    await expect(page.locator(checkVerticalGroup2)).not.toBeChecked();
+    await expect(page.locator(checkVerticalGroup3)).not.toBeChecked();
+    await expect(page.locator(checkVerticalGroup4)).not.toBeChecked();
+    await expect(page.locator(checkHorizontalGroup1)).not.toBeChecked();
+    await expect(page.locator(checkHorizontalGroup2)).not.toBeChecked();
+    await expect(page.locator(checkHorizontalGroup3)).not.toBeChecked();
+    await expect(page.locator(checkHorizontalGroup4)).not.toBeChecked();
+
+    console.log("All 9 checkboxes are visible and the first checkbox can be checked and unchecked successfully.");
+
+    console.log("Will attempt to click a disabled checkbox and expect an error to be thrown.");
+
+    // Below lines should TRIGGER ERRORS BECAUSE THE CHECKBOXES ARE DISABLED
+    //await page.locator(checkVerticalGroup3).click({timeout: 1_000}); // check SHOULD TRIGGER ERROR BECAUSE IT IS DISABLED
+    //await page.locator(checkVerticalGroup4).click({timeout: 100}); // check SHOULD TRIGGER ERROR BECAUSE IT IS DISABLED
+  });
+
+  test('test wednesday aug 26 - RADIOS', async ({ page }) => {
+    await page.goto(url2);
+
+    const radioMale: string = "input[value='Male'][name='gender']";
+    const radioFemale: string = "input[value='Female'][name='gender']";
+    const radioOther: string = "input[value='Other'][name='gender']";
+
+    // All radio buttons should be visible and enabled
+    await expect(page.locator(radioMale)).toBeVisible();
+    await expect(page.locator(radioFemale)).toBeVisible();
+    await expect(page.locator(radioOther)).toBeVisible();
+    await expect(page.locator(radioMale)).toBeEnabled();
+    await expect(page.locator(radioFemale)).toBeEnabled();
+    await expect(page.locator(radioOther)).toBeEnabled();
+
+    await page.locator(radioMale).click();
+    await page.locator(radioOther).click();
+
+    // Only 1 should be checked
+    await expect(page.locator(radioMale)).not.toBeChecked();
+    await expect(page.locator(radioOther)).toBeChecked();   
+
+  });
+
+  test('test wednesday aug 26 - SELECT DROPDOWN', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.fill('#user-name', 'standard_user');
+    await page.fill('#password', 'secret_sauce');
+    await page.click('#login-button');
+
+    // default sorting is 'name ASCENDING'
+
+    await page.locator('.product_sort_container').selectOption('hilo'); // 'price DESCENDING'
+  });
 });
+
+
